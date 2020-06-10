d0a779aefaff2f240c0bb20a4ae3eb801bc1e573
larrykim <foflzla12@gmail.com>
2020-06-10 17:08:34 +0900

    mock 테스트

--- src/test/java/com/larrykim/jpastudy/ControllerTest.java
+++ src/test/java/com/larrykim/jpastudy/ControllerTest.java
@@ -1,24 +1,26 @@
 package com.larrykim.jpastudy;
 
-import com.larrykim.jpastudy.web.ItemController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = {ItemController.class})
+@SpringBootTest
+@AutoConfigureMockMvc
+//@WebMvcTest(controllers = {ItemController.class})
 public class ControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void 아이템_컨트롤러_테스트() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/item/new"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/items/new"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
